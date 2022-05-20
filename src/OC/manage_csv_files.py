2de70650f5279a2ab33a327449cd71b9012e1992
AlessandroBertozzi <alessandro.bertozzi7@studio.unibo.it>
2022-05-20 22:06:29 +0200

    Added several parts with comments

--- src/OC/manage_csv_files.py
+++ src/OC/manage_csv_files.py
@@ -8,6 +8,7 @@ from os import listdir
 def main(data_json, input_path_zip_file, output_file_path_general_dict, output_file_path_open_cit_json):
     list_zip_file = [f for f in listdir(input_path_zip_file)]
     general_count_dict = dict()
+    # iterate all over zip file
     for zip_file in list_zip_file:
         with zipfile.ZipFile(f"{input_path_zip_file}/{zip_file}", "r") as zfile:
 
@@ -19,6 +20,7 @@ def main(data_json, input_path_zip_file, output_file_path_general_dict, output_f
                 # convert the file from bytes
                 zfiledata = BytesIO(zfile.read(name_file))
 
+                # convert the readed file to a Pandas dataframe
                 df = pd.read_csv(zfiledata)
 
                 my_dfs = {"cited": df[df["cited"].isin(data_json.keys())],
@@ -27,48 +29,74 @@ def main(data_json, input_path_zip_file, output_file_path_general_dict, output_f
 
                 for name_df, df in my_dfs.items():
                     if name_df == 'cited':
-                        # we create a new column journal in order to group by journal. We take dois from column and
-                        # put the corrisponded value from json file
+                        # we create a new column "journal" in order to apply a groupBy operation for each journal.
+                        # for each record we put a new value in the new column with the correspondent journal
+                        # from DOAJ (the json in input).
                         df["journal"] = df["cited"].apply(lambda x: data_json[x])
+                        # groupBy and count of the column cited
                         df_count = df.groupby(["journal"], as_index=False)["cited"].count()
+
+                        # Then we transform our dataframe in a dict for adding its values to a general dict
                         general_count_dict = counter_function(df_count.to_dict(orient="records"), general_count_dict,
                                                               "cited")
 
                     elif name_df == 'citing':
+                        # we create a new column "journal" in order to apply a groupBy operation for each journal.
+                        # for each record we put a new value in the new column with the correspondent journal
+                        # from DOAJ (the json in input).
                         df["journal"] = df["citing"].apply(lambda x: data_json[x])
+                        # groupBy and count of the column citing
                         df_count = df.groupby(["journal"], as_index=False)["citing"].count()
+
+                        # Then we transform our dataframe in a dict for adding its values to a general dict
                         general_count_dict = counter_function(df_count.to_dict(orient="records"), general_count_dict,
                                                               "citing")
 
 
 
                     elif name_df == "open_cit":
-
+                        # we create a deep copy of the dataframe for using it for another computation
+                        copy_df = df.copy()
+                        # we create a new column "journal" in order to apply a groupBy operation for each journal.
+                        # for each record we put a new value in the new column with the correspondent journal
+                        # from DOAJ (the json in input). Then we rename the columns for creating a new field
+                        # in the general dict
                         df["journal"] = df["cited"].apply(lambda x: data_json[x])
-                        df = df.rename(columns={'journal': 'journal', 'cited':'open_cit', 'citing':'citing'})
-                        # here we need a renaming because we want to use cited column but on a different df, which
-                        # have
-                        df_count = df.groupby(["journal"], as_index=False)["open_cit"].count()
+                        df = df.rename(columns={'journal': 'journal', 'cited':'open_cited', 'citing':'citing'})
+                        # groupBy and count of the column open_cited
+                        df_count = df.groupby(["journal"], as_index=False)["open_cited"].count()
+                        # Then we transform our dataframe in a dict for adding its values to a general dict
                         general_count_dict = counter_function(df_count.to_dict(orient="records"), general_count_dict,
-                                                              "open_cit")
+                                                              "open_cited")
 
-                    with open(f"{output_file_path_open_cit_json}/general_count_dict.json",
+
+                        # we create a new column "journal" in order to apply a groupBy operation for each journal.
+                        # for each record we put a new value in the new column with the correspondent journal
+                        # from DOAJ (the json in input). Then we rename the columns for creating a new field
+                        # in the general dict
+                        copy_df["journal"] = copy_df["citing"].apply(lambda x: data_json[x])
+                        # groupBy and count of the column open_citing
+                        copy_df = copy_df.rename(columns={'journal': 'journal', 'cited':'cited', 'citing':'open_citing'})
+                        df_count = copy_df.groupby(["journal"], as_index=False)["open_citing"].count()
+                        # Then we transform our dataframe in a dict for adding its values to a general dict
+                        general_count_dict = counter_function(df_count.to_dict(orient="records"), general_count_dict,
+                                                              "open_citing")
+
+                        # we save for each iteration the df filtered by DOAJ dois on both columns (cited and citing)
+                        # in a json format
+                        result = df.to_dict(orient="records")
+                        json_object = json.dumps(result)
+                        with open(f"{output_file_path_open_cit_json}/{name_df}/{name_file.replace('.csv', '').replace(':', '_')}.json",
+                                  "w") as outfile:
+                            outfile.write(json_object)
+                            outfile.close()
+
+                    # we save for each iteration the the general dict
+                    with open(f"{output_file_path_general_dict}/general_count_dict_prova.json",
                               "w") as outfile:
                         json_object = json.dumps(general_count_dict)
                         outfile.write(json_object)
                         outfile.close
-
-
-                        # result = df.to_dict(orient="records")
-                        # json_object = json.dumps(result)
-                        # with open(f"E:/{name_df}/{name_file.replace('.csv', '').replace(':', '_')}.json",
-                        #           "w") as outfile:
-                        #     outfile.write(json_object)
-                        #     outfile.close()
-
-
-
-
 
 
 def counter_function(group_by_dict, json_file, name):
@@ -78,7 +106,8 @@ def counter_function(group_by_dict, json_file, name):
             json_file[record["journal"]] = dict()
             json_file[record["journal"]]["cited"] = 0
             json_file[record["journal"]]["citing"] = 0
-            json_file[record["journal"]]["open_cit"] = 0
+            json_file[record["journal"]]["open_cited"] = 0
+            json_file[record["journal"]]["open_citing"] = 0
         json_file[record["journal"]][name] += record[name]
 
     return json_file
