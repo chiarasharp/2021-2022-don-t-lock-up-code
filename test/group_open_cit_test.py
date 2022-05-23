34d21cd8248b9662c202fc024210d3f764f853a0
Chiara Manca <48201839+chiarasharp@users.noreply.github.com>
2022-05-23 13:56:59 +0200

    test folder moved, test group open cit ok

--- test/group_open_cit_test.py
+++ test/group_open_cit_test.py
@@ -1,13 +1,14 @@
 import os
 import unittest
 import json
-from .udf.group_open_cit import group_open_cit
+
+from src.udf.group_open_cit import group_open_cit
 
 
 class GroupOpenCitTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.open_cit_test_files_path = "./open_cit"
+        self.open_cit_test_files_path = "test/open_cit"
         self.open_cit_years_path = "open_cit_in_years.json"
         self.open_cit_err_path = "open_cit_w_date_err.json"
         self.num_of_open_cit_err = 6
@@ -24,8 +25,8 @@ class GroupOpenCitTestCase(unittest.TestCase):
         self.assertEqual(len(open_cit_years), self.num_of_open_cit_years)
 
         f = open(self.open_cit_err_path)
-        open_cit_years = json.load(f)
-        self.assertEqual(len(open_cit_years), self.num_of_open_cit_years)
+        open_cit_err = json.load(f)
+        self.assertEqual(len(open_cit_err), self.num_of_open_cit_err)
 
 
 if __name__ == '__main__':
