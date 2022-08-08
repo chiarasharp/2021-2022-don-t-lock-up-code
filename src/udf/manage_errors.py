9474c45742c3b917916752367fb3f9b744265791
Chiara Manca <48201839+chiarasharp@users.noreply.github.com>
2022-08-08 16:09:47 +0200

    code for managing errors files
    to count the elements of each category of errors (to be updated for counting the errors of missing dois etc if needed)

---
+++ src/udf/manage_errors.py
@@ -0,0 +1,23 @@
+from glob import glob
+import os
+import json
+
+titles = []
+count = []
+
+# getting the json files with the errors
+all_files = glob(os.path.join("errors", "*.json"))
+
+for f in all_files:
+    # getting the titles of the files
+    titles.append(f[7:-5])
+    with open(f) as inf:
+        j = json.load(inf)
+
+        # counting the elements inside the files
+        count.append(len(j))
+
+# creating a dictionary of the count of each category of errors and dumping it in a json
+dictionary = dict(zip(titles, count))
+with open("errors.json", "w") as outfile:
+    json.dump(dictionary, outfile)
