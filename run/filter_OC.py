51bfac8ba4c06e44bcc536ca2023be443a499dc1
Alessandro Bertozzi <alessandro.bertozzi7@studio.unibo.it>
2022-11-11 00:55:34 +0100

    changes to filter_OC

--- run/filter_OC.py
+++ run/filter_OC.py
@@ -1,15 +1,11 @@
-import time
 import pandas as pd
 import pickle
-from src.OC.OC_filter import manage_csv_files
+from src.OC import zip_manager, csv_manager
 import os
-from glob import glob
-from tqdm import tqdm
-import zipfile
 
 pd.options.mode.chained_assignment = None
 
-"""
+'''
 |--------------------------------------------------------------------------
 | DESCRIPTION
 |--------------------------------------------------------------------------
@@ -19,74 +15,69 @@ REQUIRED SCRIPT: /
 The script iterates all over the files filtered from OpenCitations on both columns cited and citing, containing the 
 DOAJ journals. The script create two different folder (cited and citing), on the specified output path.
 
-"""
+'''
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    """
+    '''
     |--------------------------------------------------------------------------
-    | INPUT VARIABLE
+    | INPUT VARIABLES
     |--------------------------------------------------------------------------
-    """
+    '''
 
-    path_zip_file = input(str("\nPath to zip OpenCitations\n\n > "))
+    path_zip_file = os.path.join('..', os.getenv('input_directory'))
 
-    path_pickle_file = input(str("\nPath to pickle_file\n\n > "))
+    path_output = os.path.join('..', os.getenv('output_directory'))
 
-    path_output = input(str("\nPath for output files\n\n > "))
+    path_pickle_file = os.path.join(path_output, 'DOAJ/doi_articles_journals.pickle')
 
-    print("\n")
-
-    """
+    '''
     |--------------------------------------------------------------------------
-    | EXECUTION
+    | LOAD
     |--------------------------------------------------------------------------
-    """
+    '''
 
     # open pickle file from previous run: it contains all the Dois divided by journals
     with open(path_pickle_file, 'rb') as pickle_file:
         data_json = pickle.load(pickle_file)
-    all_zips = glob(os.path.join(path_zip_file, "*.zip"))
 
-    # iterate all over the list with zip files until it is empty
-    while len(all_zips) != 0:
-        dir_zip_name = all_zips.pop(0)
-        print(f"starting with another zip dir: {dir_zip_name}")
-        # extrac all files from zip dir in another dir
-        with zipfile.ZipFile(dir_zip_name, 'r') as zip_ref:
-            zip_ref.extractall("../data/prova")
-        zip_ref.close()
+    # create a temporary folder
+    if not os.path.exists(os.path.join(path_output, 'tmp')):
+        os.mkdir(os.path.join(path_output, 'tmp'))
 
-        # take all files from the repository created above
-        all_files = glob(os.path.join("../data/prova", "*.csv"))
-        # iterate all over the CSV files
-        for csv in tqdm(all_files):
-            name_file = csv.split("\\")[-1]
+    '''
+    |--------------------------------------------------------------------------
+    | EXECUTION
+    |--------------------------------------------------------------------------
+    '''
 
-            # convert the read file to a Pandas dataframe
-            df = pd.read_csv(csv,
-                             dtype={'timespan': str,
-                                    'journal_sc': str,
-                                    'author_sc': str,
-                                    'creation': str,
-                                    'cited': str,
-                                    "citing": str,
-                                    "oci": str})
+    for csv in zip_manager.ZipIterator(path_zip_file, os.path.join(path_output, 'tmp')):
+        name_file = csv.split('\\')[-1]
 
-            # filter the CSV records which contain the DOAJ journals on cited column
-            df_cited = manage_csv_files(data_json, df, "cited")
-            df_cited.to_csv(path_output + f"/cited/{name_file}")
+        # convert the read file to a Pandas dataframe
+        df = pd.read_csv(csv, dtype={
+            'timespan': str,
+            'journal_sc': str,
+            'author_sc': str,
+            'creation': str,
+            'cited': str,
+            'citing': str,
+            'oci': str
+        })
 
-            # filter the CSV records which contain the DOAJ journals on citing column
-            df_citing = manage_csv_files(data_json, df, "citing")
-            df_citing.to_csv(path_output + f"/citing/{name_file}")
+        # filter the CSV records which contain the DOAJ journals on cited column
+        df_cited = csv_manager.refine(df, ['oci', 'creation', 'cited'], 'cited', data_json)
 
-            # delete the csv file
-            os.remove(csv)
+        df_cited = csv_manager.add_journal(df_cited, "cited", data_json)
 
-        # completed the iteration over the zip repo, delete it
-        os.remove(dir_zip_name)
+        # filter the CSV records which contain the DOAJ journals on citing column
+        df_citing = csv_manager.refine(df, ['oci', 'creation', 'citing'], 'citing', data_json)
 
-        # take a rest
-        print(f"taking 200 second of rest")
-        time.sleep(30)
+        df_citing = csv_manager.add_journal(df_citing, "citing", data_json)
+
+        df_result = df_citing.merge(df_cited, how='outer').reset_index(drop=True).convert_dtypes().drop_duplicates()
+
+        df_result.to_csv(os.path.join(path_output, f'OC/filtered/{name_file}'), index=False)
+
+        # delete the csv file
+        os.remove(csv)
