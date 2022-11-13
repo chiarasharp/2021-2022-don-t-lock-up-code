4fc93477d18c015936eed650a7d4f25bdca12ab0
Alessandro Bertozzi <alessandro.bertozzi7@studio.unibo.it>
2022-11-13 17:24:36 +0100

    new version of bat files

--- run.bat
+++ run.bat
@@ -4,11 +4,13 @@ title OpenCitations-pipeline
 python src/udf/read_env.py .env
 cd run
 
-::python -m filter_OC
-python -m groupBy_run
-python -m concat_run
+
+:: python -m 1_divide_dois_by_journals_DOAJ
+:: python -m 2_filter_OC
+python -m 3_groupBy_OC
+python -m 4_concat_groupBy_OC
+python -m 5_make_ratio
 
 cd ..
-::venv\Scripts\deactivate.bat
 
 
--- start-venv.bat
+++ start-venv.bat
@@ -1,23 +1,14 @@
 @echo OFF
-echo What is the input directory path ?:
-set /p input_directory=
-echo What is the output directory path ?:
-set /p output_directory=
+if not exist .env (
 
-python src/udf/create_env_file.py %input_directory% %output_directory%
+python src/udf/create_env_and_directories.py
 
-if not exist "%output_directory%"  mkdir "%output_directory%"
-if not exist "%output_directory%/errors"  mkdir "%output_directory%/errors"
-if not exist "%output_directory%/errors/null"  mkdir "%output_directory%/errors/null"
-if not exist "%output_directory%/errors/wrong"  mkdir "%output_directory%/errors/wrong"
-if not exist "%output_directory%/DOAJ"  mkdir "%output_directory%/DOAJ"
-if not exist "%output_directory%/OC"  mkdir "%output_directory%/OC"
-if not exist "%output_directory%/OC/filtered"  mkdir "%output_directory%/OC/filtered"
-if not exist "%output_directory%/OC/group_by_year"  mkdir "%output_directory%/OC/group_by_year"
-if not exist "%output_directory%/OC/group_by_year/normal"  mkdir "%output_directory%/OC/group_by_year/normal"
-if not exist "%output_directory%/OC/group_by_year/by_journal"  mkdir "%output_directory%/OC/group_by_year/by_journal"
+) else (
+
+echo .env file already exist. If you want to recreate it, delete it
+
+)
 
 
+echo start the Virtual Environment...
 venv\Scripts\activate
-
-
