92687734c9303132e9b7655358c95500f08f676c
Alessandro Bertozzi <alessandro.bertozzi7@studio.unibo.it>
2022-11-13 17:25:17 +0100

    update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="my-project",
+    version="1.0.0",
+    author="You",
+    author_email="you@example.com",
+    description="This is my project",
+    packages=["src", "run"],
+)
