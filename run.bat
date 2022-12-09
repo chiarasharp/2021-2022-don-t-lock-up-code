f0583f3bdbf23fac2c0f87e1bbc04bfaf68bbcff
Alessandro Bertozzi <alessandro.bertozzi7@studio.unibo.it>
2022-12-09 11:02:29 +0100

    Last update: README file, setup, run file

--- run.bat
+++ run.bat
@@ -3,11 +3,12 @@ title OpenCitations-pipeline
 
 cd run
 
-:: python -m 1_divide_dois_by_journals_DOAJ
-:: python -m 2_filter_OC
+python -m 1_divide_dois_by_journals_DOAJ
+python -m 2_filter_OC
 python -m 3_groupBy_OC
 python -m 4_concat_groupBy_OC
 python -m 5_make_ratio
+python -m 6_add_useful_metrics
 
 cd ..
 
